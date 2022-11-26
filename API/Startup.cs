813f0d0a3db3fd2bd01c5b94de6425472720190c
MSM <MonMail@yahoo.fr>
2022-11-26 15:01:44 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,12 +31,15 @@ namespace API
             services.AddDbContext<DataContext>(options=>{
                 //options.UseSqlite("Data source=datingapp.db");
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
+            });           
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +57,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseEndpoints(endpoints =>
             {
